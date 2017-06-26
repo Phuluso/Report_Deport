c2f4c141ae5574565dad40f42b7d4258bbc157dc
ngwphu001@myuct.ac.za <ngwphu001@myuct.ac.za>
2017-06-26 14:23:58 +0200

    Login page redirect changed

--- ReportDeport/Controllers/AccountController.cs
+++ ReportDeport/Controllers/AccountController.cs
@@ -165,7 +165,7 @@ namespace ReportDeport.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("UserLogin", "Home");
                 }
                 AddErrors(result);
             }
@@ -394,7 +394,7 @@ namespace ReportDeport.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("UserLogin", "Home");
         }
 
         //
@@ -451,7 +451,7 @@ namespace ReportDeport.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("UserLogin", "Home");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
